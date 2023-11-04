79547df94b0554416f8ccc52fc088ad2b48ae7e5
Baher Tamer <baher.appledev@gmail.com>
2023-11-04 13:11:24 +0200

    Setup Xcode files

---
+++ DoMatrix/DoMatrix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DoMatrix
+//
+//  Created by Baher Tamer on 04/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DoMatrix/DoMatrix/DoMatrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoMatrixApp.swift
+//  DoMatrix
+//
+//  Created by Baher Tamer on 04/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct DoMatrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
