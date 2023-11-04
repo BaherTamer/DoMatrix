cb43ff466905603baaeeed5170b03e8e22ce08a1
Baher Tamer <baher.appledev@gmail.com>
2023-11-04 14:16:31 +0200

    Create TaskDetailsView.swift

---
+++ DoMatrix/DoMatrix/Presentation/Scenes/Task Details/Views/TaskDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskDetailsView.swift
+//  DoMatrix
+//
+//  Created by Baher Tamer on 04/11/2023.
+//
+
+import SwiftUI
+
+struct TaskDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskDetailsView()
+}
