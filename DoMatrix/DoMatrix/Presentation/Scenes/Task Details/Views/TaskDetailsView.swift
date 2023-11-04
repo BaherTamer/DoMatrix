619bcfc0952a15f5d804d2c64828040c3093726a
Baher Tamer <baher.appledev@gmail.com>
2023-11-04 15:19:34 +0200

    Create TaskDetailsView.swift

--- DoMatrix/DoMatrix/Presentation/Scenes/Task Details/Views/TaskDetailsView.swift
+++ DoMatrix/DoMatrix/Presentation/Scenes/Task Details/Views/TaskDetailsView.swift
@@ -5,14 +5,93 @@
 //  Created by Baher Tamer on 04/11/2023.
 //
 
+import OSLog
 import SwiftUI
 
 struct TaskDetailsView: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.modelContext) private var modelContext
+    
+    @State private var task: DMTask = .init()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                taskInfoSection
+                taskPrioritySection
+            }
+            .navigationTitle("Details")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel") {
+                        cancelButtonPressed()
+                    }
+                }
+                
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Done") {
+                        doneButtonPressed()
+                    }
+                }
+            }
+        }
     }
 }
 
-#Preview {
-    TaskDetailsView()
+// MARK: - Variables
+extension TaskDetailsView {
+    // Task info section
+    private var taskInfoSection: some View {
+        Section {
+            TextField("Title", text: $task.title)
+        }
+    }
+    
+    // Task priority section
+    private var taskPrioritySection: some View {
+        Section {
+            
+            DetailsToggleRow(isOn: $task.isUrgent, title: "Urgent", systemImage: SFSymbols.toggleUrgent, imageColor: .pink)
+            
+            DetailsToggleRow(isOn: $task.isImportant, title: "Important", systemImage: SFSymbols.toggleImportant, imageColor: .orange)
+            
+        } footer: {
+            Text("Task priority: \(priorityFooter)")
+        }
+    }
+    
+    private var priorityFooter: Text {
+        Text(task.priority.title)
+            .fontWeight(.semibold)
+            .foregroundStyle(task.priority.color)
+    }
+}
+
+// MARK: - Functions
+extension TaskDetailsView {
+    private func cancelButtonPressed() {
+        dismiss()
+    }
+    
+    private func doneButtonPressed() {
+        if task.title.isEmpty {
+            task.title = "New Task"
+        }
+        
+        withAnimation {
+            modelContext.insert(task)
+        }
+        
+        dismiss()
+    }
+    
+    private func saveContext() {
+        do {
+            try modelContext.save()
+        } catch {
+            Logger.saveContext.error("Failed to save the task, \(error).")
+        }
+    }
 }
