c0e3cc9e0229f8b05a8be5017e3377de392a2edf
Baher Tamer <baher.appledev@gmail.com>
2023-11-04 13:30:08 +0200

    Create DMPriority.swift

---
+++ DoMatrix/DoMatrix/Models/DMPriority.swift
@@ -0,0 +1,18 @@
+//
+//  DMPriority.swift
+//  DoMatrix
+//
+//  Created by Baher Tamer on 04/11/2023.
+//
+
+import SwiftData
+import SwiftUI
+
+enum DMPriority: Int, CaseIterable {
+    
+    case doFirst = 1
+    case schedule
+    case delegate
+    case dontDo
+    
+}
