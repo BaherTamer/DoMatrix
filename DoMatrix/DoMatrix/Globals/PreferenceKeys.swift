8256e1b7ad432c8ff42c5795fece5ffff6069657
Baher Tamer <baher.appledev@gmail.com>
2023-11-05 14:35:24 +0200

    Create PreferenceKeys.swift

---
+++ DoMatrix/DoMatrix/Globals/PreferenceKeys.swift
@@ -0,0 +1,12 @@
+//
+//  PreferenceKeys.swift
+//  DoMatrix
+//
+//  Created by Baher Tamer on 05/11/2023.
+//
+
+import Foundation
+
+struct PreferenceKeys {
+    static let showCompletedTasks = "showCompletedTasks"
+}
